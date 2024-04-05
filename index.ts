fc48987ed1cbc969522429d05ea6666e6186a60b
tayyabalidev <163244910+tayyabalidev@users.noreply.github.com>
2024-04-05 03:44:02 -0700

    currency

---
+++ index.ts
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+let Converstion ={
+    "PKR" : {
+        "USD" :0.0044,
+        "GBP" :0.0037,
+        "PKR" :1,
+    },
+    "GBP" : {
+        "USD" :1.21,
+        "PKR" :271.79,
+        "GBP" :1,
+    },
+    "USD" : {
+        "PKR" :225.50,
+        "GBP" :0.83,
+        "USD" :1,
+       },
+}
+const answer :{
+    form: "PKR" | "USD" | "GBP",
+    To: "PKR" | "USD" | "GBP",
+    amount:number,
+}
+ = await inquirer.prompt([
+    {
+        type : "list",
+        name : "form",
+        choices :["PKR","USD","GBP"],
+        message :"select your Currency :"
+    },
+    {
+        type : "list",
+        name : "To",
+        choices :["PKR","USD","GBP"],
+        message :"select your conservtion Currency :"
+    },
+    {
+        type : "number",
+        name : "amount",
+        message :"Enter your amount :"
+    },
+]);
+const {form, To, amount}=answer;
+if(form && To && amount){
+let result=Converstion[form][To] * amount;
+console.log(`your converstion form ${form} To ${To} is ${result}`)
+}else{
+    console.log("invalid inputs")
+}
